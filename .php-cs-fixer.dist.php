72dfedb66870ae67459937e6bb0fcc3bc7076833
Mihail Ryazantsev <ryazantsev_ms@groupbwt.com>
2023-10-31 03:54:07 +0300

    feat: added cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+include __DIR__ . '/vendor/autoload.php';
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(['src'])
+    ->in(['tests'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'no_alternative_syntax' => true,
+        'strict_comparison' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'yoda_style' => false,
+    ])
+    ->setFinder($finder)
+    ->setUsingCache(false)
+    ->setRiskyAllowed(true)
+;
